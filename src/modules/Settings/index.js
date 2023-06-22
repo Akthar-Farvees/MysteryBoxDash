d4ae77ac0db7ecfb6ebd3b11937321a511bd1dde
Akthar farvees <junaideenakthar123@gmail.com>
2023-06-22 22:19:20 +0530

    Built Setting Page

--- src/modules/Settings/index.js
+++ src/modules/Settings/index.js
@@ -4,7 +4,17 @@ import { Card, Form, Input, Button } from 'antd'
 const Settings = () => {
   return (
     <Card title="Settings" style={{ margin: 20, }}>
-        Hello
+        <Form layout='vertical' wrapperCol={{span: 8}}>
+            <Form.Item label="Restaurant Name" required>
+                <Input placeholder='Enter restaurant name'/>
+            </Form.Item>
+            <Form.Item label="Restaurant Address" required></Form.Item>
+
+            <Form.Item>
+                <Button type='primary'>Submit</Button>
+            </Form.Item>
+            
+        </Form>
     </Card>
 
   )
