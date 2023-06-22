62ecc2e27b1ae3ab1968c922079f0f6adb26c290
Akthar farvees <junaideenakthar123@gmail.com>
2023-06-22 15:54:35 +0530

    Created Side Bar With Two Screens

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import DetailedOrder from "./modules/DetailedOrder";
 import Orders from "./modules/Orders";
 import { Route, Routes } from "react-router-dom";
+import { Layout, Image, Space } from "antd";
+import imageSrc from './assests/images/MB Logo.png';
+
+
+const { Sider, Content, Footer } = Layout;
 
 function App() {
   return (
-   <Routes>
-    <Route path="" element={ <Orders/> } />
-    <Route path="order/:id" element={ <DetailedOrder/> } />
-   </Routes>
+
+    <Layout>
+      <Sider style={{ height: '100vh', backgroundColor: "white" }}>
+        <Image src={imageSrc} preview={false} />
+      </Sider>
+      <Layout>
+        <Content style={{  }}>
+          <Routes>
+            <Route path="" element={<Orders />} />
+            <Route path="order/:id" element={<DetailedOrder />} />
+          </Routes>
+        </Content>
+        <Footer style={{ textAlign: 'center'}}>
+          &copy; 2023 Mystery Box. All rights reserved.<br />
+          Designed and developed by <a href="https://www.linkedin.com/in/aktharfarvees/" style={{
+            color: 'black',
+            fontWeight: "600",
+            padding: 3,
+            borderRadius: 3,
+          }}>Akthar Farvees</a>.
+        </Footer>
+      </Layout>
+    </Layout>
+
+
   );
 }
 
