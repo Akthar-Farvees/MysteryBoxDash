0efeb74c032559752b8bd3a33c6301217d6e6b09
Akthar farvees <junaideenakthar123@gmail.com>
2023-06-22 22:55:36 +0530

    set the lat and lng with the help of google place api

--- src/modules/Settings/index.js
+++ src/modules/Settings/index.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, {useState} from 'react' 
 import { Card, Form, Input, Button } from 'antd'
-import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
+import GooglePlacesAutocomplete, {
+  geocodeByAddress, 
+  getLatLng} 
+  from 'react-google-places-autocomplete'
 
 const Settings = () => {
+  const [address, setAddress] = useState(null);
+  const [coordinates, setCoordinates] = useState(null);
+
+  const getAddressLatLng = async (address) => {
+    setAddress(address);
+
+    const GeocodeByAddress = await geocodeByAddress(address.label);
+    const LatLng = await getLatLng(GeocodeByAddress[0]);
+    console.log(LatLng );
+    setCoordinates(LatLng);
+  }
+
   return (
     <Card title="Settings" style={{ margin: 20, }}>
         <Form layout='vertical' wrapperCol={{span: 8}}>
@@ -11,13 +26,18 @@ const Settings = () => {
             </Form.Item>
             <Form.Item label="Restaurant Address" required>
               <GooglePlacesAutocomplete
-              apiKey='AIzaSyAZ5ZypfUXy_LSA4Ul4DTfg9t9hGWzaMts'/>
+              apiKey='AIzaSyAZ5ZypfUXy_LSA4Ul4DTfg9t9hGWzaMts'
+              selectProps={{
+                value: address,
+                onChange: getAddressLatLng
+              }}
+              />
             </Form.Item>
 
             <Form.Item>
                 <Button type='primary'>Submit</Button>
             </Form.Item>
-            
+            <span>Lattitude: {coordinates?.lat} - Logitude: {coordinates?.lng}</span>
         </Form>
     </Card>
 
