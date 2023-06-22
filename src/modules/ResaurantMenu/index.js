e48a355ab063b92b44366a9d9f5d315f02779290
Akthar farvees <junaideenakthar123@gmail.com>
2023-06-22 18:55:49 +0530

    Created RestaurantMenu file

---
+++ src/modules/ResaurantMenu/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Card, Layout } from 'antd';
+import dishes from '../../assests/data/dishes.json'
+
+const RestaurantMenu = () => {
+  return (
+    <Card>
+        
+    </Card>
+  )
+}
+
+export default RestaurantMenu
