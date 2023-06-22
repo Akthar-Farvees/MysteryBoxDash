da5c28023dac86b5431e75e973f9f560f5874d5d
Akthar farvees <junaideenakthar123@gmail.com>
2023-06-22 21:57:04 +0530

    Created Settings Screen

--- src/Components/AppRoutes/index.js
+++ src/Components/AppRoutes/index.js
@@ -4,6 +4,7 @@ import Orders from "../../modules/Orders/index";
 import RestaurantMenu from '../../modules/RestaurantMenu/index.js';
 import  CreateMenuItem from '../../modules/CreateMenuItem/index';
 import  OrderHistory from '../../modules/OrderHistory/index';
+import  Settings from '../../../src/modules/Settings/index';
 import { Route, Routes } from "react-router-dom";
 
 function AppRoutes() {
@@ -14,6 +15,7 @@ function AppRoutes() {
     <Route path="menu" element={<RestaurantMenu/>} />
     <Route path="menu/create" element={<CreateMenuItem/>} />
     <Route path="order-history" element={<OrderHistory/>} />
+    <Route path="settings" element={<Settings/>} />
   </Routes>
   )
 }
---
+++ src/modules/Settings/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Card, Form, Input, Button } from 'antd'
+
+const Settings = () => {
+  return (
+    <Card title="Settings" style={{ margin: 20, }}>
+        Hello
+    </Card>
+
+  )
+}
+
+export default Settings
