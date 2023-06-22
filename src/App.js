6f0c3986307d3ab62ad542269d6f6df843aea08f
Akthar farvees <junaideenakthar123@gmail.com>
2023-06-22 14:25:36 +0530

    Added routes and route to navigate actual page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import DetailedOrder from "./modules/DetailedOrder";
 import Orders from "./modules/Orders";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <Orders/>
+   <Routes>
+    <Route path="" element={ <Orders/> } />
+    <Route path="order/:id" element={ <DetailedOrder/> } />
+   </Routes>
   );
 }
 
